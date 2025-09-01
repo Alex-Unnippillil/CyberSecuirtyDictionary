bb0f279f4c73716810ea5beb1654f6b9914a0ce3
Alex-Unnippillil <Alex-Unnippillil@users.noreply.github.com>
2025-09-01 22:32:12 +0000

    deploy: 07b16426e054c72d663d89063d9daba8c2dae606

---
+++ word/[slug]/page.tsx
@@ -0,0 +1,17 @@
+import PosTabs from '../../components/PosTabs';
+
+interface WordPageProps {
+  params: {
+    slug: string;
+  };
+}
+
+export default function WordPage({ params }: WordPageProps) {
+  return (
+    <div>
+      <h1>{params.slug}</h1>
+      <PosTabs slug={params.slug} />
+    </div>
+  );
+}
+
