7b6ccacc8f9b145c33f961343e02870bcc262a16
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-09-01 14:15:26 -0700

    feat: centralize motion tokens

---
+++ tokens/motion.js
@@ -0,0 +1,12 @@
+export const duration = {
+  fast: 0.15,
+  base: 0.25,
+  slow: 0.4,
+};
+
+export const easing = {
+  linear: "linear",
+  in: "cubic-bezier(0.4, 0, 1, 1)",
+  out: "cubic-bezier(0, 0, 0.2, 1)",
+  inOut: "cubic-bezier(0.4, 0, 0.2, 1)",
+};
