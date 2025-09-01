6672860e612dfb11905b7c71814121fa8e49baa7
Alex-Unnippillil <Alex-Unnippillil@users.noreply.github.com>
2025-09-01 22:32:25 +0000

    deploy: 3efad6377d0b177fa776f5a1bbd5b63ef39c3351

---
+++ components/results/DefinitionItem.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import highlight from "../../lib/highlight";
+
+interface DefinitionItemProps {
+  term: string;
+  definition: string;
+  match?: string;
+}
+
+export default function DefinitionItem({
+  term,
+  definition,
+  match,
+}: DefinitionItemProps) {
+  const title = match ? highlight(term, match) : term;
+  const body = match ? highlight(definition, match) : definition;
+
+  return (
+    <div className="definition-item">
+      <h3 dangerouslySetInnerHTML={{ __html: title }} />
+      <p dangerouslySetInnerHTML={{ __html: body }} />
+    </div>
+  );
+}
