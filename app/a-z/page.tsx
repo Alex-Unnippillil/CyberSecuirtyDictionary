3ea3bdea73730d66eab0dbbd4b5ef8a9ec06cd1c
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-09-01 14:08:49 -0700

    feat: add A-Z index for terms

---
+++ app/a-z/page.tsx
@@ -0,0 +1,7 @@
+import AZIndex from "../../components/search/AZIndex";
+import termsData from "../../terms.json";
+
+export default function AZPage() {
+  return <AZIndex terms={termsData.terms} />;
+}
+
