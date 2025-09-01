f2f8f8cbb225d8278d7bc2e5c1e630a53a3ae06f
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-09-01 14:09:06 -0700

    feat: add error page with reload link

---
+++ app/error.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+export default function Error() {
+  return (
+    <div className="p-6">
+      <h2>Something went wrong</h2>
+      <p>
+        <a
+          href="#"
+          onClick={(e) => {
+            e.preventDefault();
+            window.location.reload();
+          }}
+        >
+          Reload page
+        </a>
+      </p>
+      <p>
+        If the issue persists, please report it on{" "}
+        <a href="https://github.com/alex-unnippillil/CyberSecuirtyDictionary/issues">
+          GitHub
+        </a>
+        .
+      </p>
+    </div>
+  );
+}
