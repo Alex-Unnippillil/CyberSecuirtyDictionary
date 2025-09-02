ac42e5eb42ccca4310ff6fc00b59729ba14db7a6
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-09-01 19:14:14 -0700

    feat: add JSON-LD structured data

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,31 @@
 import SearchBar from "../components/search/SearchBar";
+import Script from "next/script";
 
 export default function Home() {
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "WebSite",
+    name: "Cyber Security Dictionary",
+    url: "https://alex-unnippillil.github.io/CyberSecuirtyDictionary/",
+    potentialAction: {
+      "@type": "SearchAction",
+      target:
+        "https://alex-unnippillil.github.io/CyberSecuirtyDictionary/?q={search_term_string}",
+      "query-input": "required name=search_term_string",
+    },
+  };
+
   return (
-    <main>
-      <h1>Cyber Security Dictionary</h1>
-      <SearchBar />
-    </main>
+    <>
+      <Script
+        id="json-ld-home"
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
+      <main>
+        <h1>Cyber Security Dictionary</h1>
+        <SearchBar />
+      </main>
+    </>
   );
 }
