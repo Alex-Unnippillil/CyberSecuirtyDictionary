96668ba4a3c2492f5de514f2517e541d4c936924
Alex Unnippillil <alex.unnippillil@hotmail.com>
2026-05-04 20:48:04 -0500

    broaden ts coverage and add typecheck gate

--- types/next-image.d.ts
+++ types/next-image.d.ts
@@ -1,2 +1,8 @@
-declare const Image: React.ComponentType<any>;
-export default Image;
+declare module 'next/image' {
+  import * as React from 'react';
+
+  export type ImageProps = React.ImgHTMLAttributes<HTMLImageElement>;
+
+  const Image: React.ComponentType<ImageProps>;
+  export default Image;
+}
--- types/next-server.d.ts
+++ types/next-server.d.ts
@@ -1,8 +1,11 @@
-export class NextResponse extends Response {
-  constructor(body?: BodyInit | null, init?: ResponseInit);
-  static json(data: any, init?: ResponseInit): NextResponse;
-  static next(): NextResponse;
-}
-export interface NextRequest extends Request {
-  nextUrl: URL;
+declare module 'next/server' {
+  export class NextResponse extends Response {
+    constructor(body?: BodyInit | null, init?: ResponseInit);
+    static json(data: unknown, init?: ResponseInit): NextResponse;
+    static next(): NextResponse;
+  }
+
+  export interface NextRequest extends Request {
+    nextUrl: URL;
+  }
 }
